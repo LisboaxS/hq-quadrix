542df3880c7d1d0c8362dead80856d9f4cb4bab1
LisboaxS <kaue.lisboa.souza@gmail.com>
2026-05-15 14:14:59 -0300

    Carrossel

---
+++ carrossel.js
@@ -0,0 +1,24 @@
+{/* <script>
+  let atual = 0;
+  const slides = document.querySelectorAll('.carrossel-slide');
+  const dots = document.querySelectorAll('.dot');
+
+  function atualizarCarrossel() {
+    document.querySelector('.carrossel-track').style.transform = `translateX(-${atual * 100}%)`;
+    dots.forEach(d => d.classList.remove('ativo'));
+    dots[atual].classList.add('ativo');
+  }
+
+  function moverCarrossel(direcao) {
+    atual = (atual + direcao + slides.length) % slides.length;
+    atualizarCarrossel();
+  }
+
+  function irParaSlide(index) {
+    atual = index;
+    atualizarCarrossel();
+  }
+
+  // Passa automaticamente a cada 4 segundos
+  setInterval(() => moverCarrossel(1), 4000);
+</script> */}
